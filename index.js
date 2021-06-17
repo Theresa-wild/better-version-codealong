e42589169be21e56525c379059fd351b9971e798
TSeewald <theresa@wild.as>
2021-06-17 20:12:16 +0200

    Add my new Commits

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 // this is fun!!! 
-// neues Kommentar
+
 
 // Das ist lustig
 
 // la le lu
 
-
+import {
+  compareAsc,
+  format,
+} from 'date-fns';
 // Today is a good day
-//hahahah haha
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+
+
+format(new Date(2014, 1, 11), 'yyyy-MM-dd')
+//=> '2014-02-11'
+
+const dates = [
+  new Date(1994, 1, 2),
+  new Date(1990, 10, 17),
+  new Date(1989, 7, 10),
+]
+dates.sort(compareAsc)
+//=> [
+//   Wed Jan 2 1994 00:00:00,
+//   Mon Oct 17 1990 00:00:00,
+//   Sun Jul node 1989 00:00:00
+// ]
+
+console.log('These are the dates:', dates);
